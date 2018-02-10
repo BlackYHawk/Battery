768267cc0082c2bd2c903af1e59d326684131602
BlackYHawk <leqinde@163.com>
2018-02-10 11:00:17 +0800

    完成电池UI

--- app/src/main/java/com/hawk/battery/MainActivity.java
+++ app/src/main/java/com/hawk/battery/MainActivity.java
@@ -1,11 +1,18 @@
 package com.hawk.battery;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.hawk.battery.widget.BatteryView;
 
 public class MainActivity extends AppCompatActivity {
+    private BatteryChangeReceiver mBatteryReceiver;
     private BatteryView ivBattery;
 
     @Override
@@ -13,6 +20,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ivBattery = (BatteryView) findViewById(R.id.ivBattery);
-        ivBattery.setPower(80);
+
+        mBatteryReceiver = new BatteryChangeReceiver();
+        IntentFilter batteryFilter = new IntentFilter();
+        batteryFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(mBatteryReceiver, batteryFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mBatteryReceiver);
+    }
+
+    private class BatteryChangeReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (Intent.ACTION_BATTERY_CHANGED.equals(action)) {
+                // 是否在充电
+                int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+                boolean isCharging = (status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                        status == BatteryManager.BATTERY_STATUS_FULL);
+                // 获取当前电量
+                int level = intent.getIntExtra("level", 0);
+                // 电量的总刻度
+                int scale = intent.getIntExtra("scale", 0);
+                // 把它转换成百分比
+                int key = ((level * 100) / scale);
+                Log.e("MainActivity", String.format("charge:%s, key:%d", String.valueOf(isCharging), key));
+                ivBattery.setPower(key);
+            }
+        }
     }
 }
--- library/src/main/java/com/hawk/battery/widget/BatteryView.java
+++ library/src/main/java/com/hawk/battery/widget/BatteryView.java
@@ -1,12 +1,16 @@
 package com.hawk.battery.widget;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 
 /**
@@ -17,12 +21,15 @@ public class BatteryView extends View {
     /**
      * 默认参数
      */
-    private static final float defaultRatio = 7f/13;                  // 电池默认高宽比例
+    private static final float defaultRatio = 7f/13;                  // 视图默认高宽比例
+    private static final float defaultWidth = 40f;                   // 视图的默认宽度
     private static final float defaultHightRatio = 2f;               // 电池与电池盖默认高比例
     private static final float defaultWidthRatio = 9f;               // 电池与电池盖默认宽比例
-    private static final float defaultHeight = 35f;                  // 视图的默认高度
-    private static final float defaultWidth = 65f;                   // 视图的默认宽度
-    private static final float defaultStroke = 3f;                   // 画笔的默认厚度
+    private static final float defaultArch = 5f;                     // 电池默认弧度
+    private static final float defaultBatteryStroke = 3f;           // 电池画笔的默认厚度
+    private static final float defaultPowerStroke = 1f;             // 电量画笔的默认厚度
+    private static final int defaultBatteryColor = Color.LTGRAY;     // 电池画笔的默认颜色
+    private static final int defaultPowerColor = Color.RED;          // 电量画笔的默认颜色
     /**
      * 画笔信息
      */
@@ -31,23 +38,27 @@ public class BatteryView extends View {
     /**
      * 视图高宽
      */
+    private int width;
     private int measureWidth;
     private int measureHeigth;
     /**
      * 电池参数
      */
-    private float mBatteryHeight = defaultHeight;                       // 电池的高度
-    private float mBatteryWidth = defaultWidth * defaultWidthRatio / (defaultWidthRatio + 1); // 电池的宽度
-    private float mCapHeight = defaultHeight / defaultHightRatio;    // 电池盖的高度
-    private float mCapWidth = defaultWidth / (defaultWidthRatio + 1);       // 电池盖的宽度
-    private float mBatteryStroke = defaultStroke;         // 画笔厚度
+    private float batteryArch = 0;                     // 电池弧度
+    private float mBatteryHeight = 0;                       // 电池的高度
+    private float mBatteryWidth = 0;                        // 电池的宽度
+    private float mCapHeight = 0;                           // 电池盖的高度
+    private float mCapWidth = 0;                            // 电池盖的宽度
+    private float mBatteryStroke = defaultBatteryStroke;         // 电池画笔厚度
+    private float mPowerStroke = defaultPowerStroke;             // 电量画笔厚度
+    private int mBatteryColor = 0;                          // 电池画笔颜色
+    private int mPowerColor = 0;                              // 电量画笔颜色
     /**
      * 电池电量
      */
-    private float mPowerPadding = 1;
     private float mPowerHeight = 0;             // 电量的总高度
     private float mPowerWidth = 0;              // 电量的总宽度
-    private float mPower = 80f;                  //当前电量
+    private float mPower = 0f;                  //当前电量
     /**
      * 矩形
      */
@@ -55,27 +66,37 @@ public class BatteryView extends View {
     private RectF mCapRect = new RectF(0, 0, 0, 0);
     private RectF mPowerRect = new RectF(0, 0, 0, 0);
 
-    public BatteryView(Context context) {
-        super(context);
-        initView();
-    }
-
     public BatteryView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initView();
+        init(attrs);
     }
 
     public BatteryView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initView();
+        init(attrs);
     }
 
-    private void initView() {
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public BatteryView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void init(AttributeSet attrs) {
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.BatteryView);
+
+        if (typedArray != null) {
+            int widthDip = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, defaultWidth, getResources().getDisplayMetrics());
+            width = typedArray.getDimensionPixelSize(R.styleable.BatteryView_batteryWidth, widthDip);
+            batteryArch = typedArray.getFloat(R.styleable.BatteryView_batteryRadius, defaultArch);
+            mBatteryColor = typedArray.getColor(R.styleable.BatteryView_batteryColor, defaultBatteryColor);
+            mPowerColor = typedArray.getColor(R.styleable.BatteryView_powerColor, defaultPowerColor);
+            typedArray.recycle();
+        }
         /**
          * 设置电池画笔
          */
         mBatteryPaint = new Paint();
-        mBatteryPaint.setColor(Color.GRAY);
+        mBatteryPaint.setColor(mBatteryColor);
         mBatteryPaint.setAntiAlias(true);
         mBatteryPaint.setStyle(Paint.Style.STROKE);
         mBatteryPaint.setStrokeWidth(mBatteryStroke);
@@ -83,62 +104,58 @@ public class BatteryView extends View {
          * 设置电量画笔
          */
         mPowerPaint = new Paint();
-        mPowerPaint.setColor(Color.RED);
+        mPowerPaint.setColor(mPowerColor);
         mPowerPaint.setAntiAlias(true);
         mPowerPaint.setStyle(Paint.Style.FILL);
-        mPowerPaint.setStrokeWidth(mBatteryStroke);
+        mPowerPaint.setStrokeWidth(mPowerStroke);
     }
 
     private void intDrawRectF() {
-        mBatteryHeight = measureHeigth;                       // 电池的高度
-        mBatteryWidth = measureWidth * defaultWidthRatio / (defaultWidthRatio + 1); // 电池的宽度
-        mCapHeight = measureHeigth / defaultHightRatio;    // 电池盖的高度
-        mCapWidth = measureWidth / (defaultWidthRatio + 1);       // 电池盖的宽度
-
-        mPowerHeight = mBatteryHeight - mBatteryStroke - mPowerPadding * 2; // 电池身体的高度
-        mPowerWidth = mBatteryWidth - mBatteryStroke - mPowerPadding * 2;// 电池身体的总宽度
+        mBatteryHeight = measureHeigth - getPaddingTop() - getPaddingBottom();       // 电池的高度
+        mBatteryWidth = (measureWidth - getPaddingLeft() - getPaddingRight()) * defaultWidthRatio / (defaultWidthRatio + 1); // 电池的宽度
+        mCapHeight = mBatteryHeight / defaultHightRatio;    // 电池盖的高度
+        mCapWidth = mBatteryWidth / (defaultWidthRatio + 1);       // 电池盖的宽度
+        mPowerHeight = mBatteryHeight - mBatteryStroke * 2; // 电量的总高度
+        mPowerWidth = mBatteryWidth - mBatteryStroke * 2;// 电量的总宽度
         /**
-         * 设置电池矩形
+         * 设置电池矩形，左对齐
          */
         mBatteryRect.left = getPaddingLeft();
-        mBatteryRect.top = 0;
-        mBatteryRect.right = (getWidth() - getPaddingRight()) + mPowerWidth;
-        mBatteryRect.bottom = mBatteryHeight;
+        mBatteryRect.top = getPaddingTop();
+        mBatteryRect.right = getPaddingLeft() + mBatteryWidth;
+        mBatteryRect.bottom = getPaddingTop() + mBatteryHeight;
         /**
          * 设置电池盖矩形
          */
-        mCapRect.left = mBatteryRect.right - mBatteryRect.left;
-        mCapRect.top = (mBatteryHeight - mCapHeight) / 2;
-        mCapRect.right = mBatteryRect.right - mBatteryRect.left + mCapWidth;
-        mCapRect.bottom = (mBatteryHeight - mCapHeight) / 2 + mCapHeight;
+        mCapRect.left = mBatteryRect.right;
+        mCapRect.top = getPaddingTop() + (mBatteryHeight - mCapHeight) / 2;
+        mCapRect.right = mBatteryRect.right + mCapWidth;
+        mCapRect.bottom = getPaddingTop() + (mBatteryHeight - mCapHeight) / 2 + mCapHeight;
         /**
          * 设置电量矩形
          */
-        mPowerRect.left = mBatteryStroke / 2 + mPowerPadding
-                + mPowerWidth * ((100f - mPower) / 100f);
-        mPowerRect.top = mPowerPadding + mBatteryStroke / 2;
-        mPowerRect.right = mBatteryStroke / 2 + mPowerPadding
-                + mPowerWidth * ((100f - mPower) / 100f);
-        mPowerRect.bottom = mBatteryStroke / 2 + mPowerPadding + mPowerHeight;
+        mPowerRect.left = getPaddingLeft() + mBatteryStroke;
+        mPowerRect.top = getPaddingTop() + mBatteryStroke;
+        mPowerRect.right = getPaddingLeft() + mBatteryStroke + mPowerWidth * mPower / 100f;
+        mPowerRect.bottom = getPaddingTop() + mBatteryStroke + mPowerHeight;
     }
 
     @Override
     protected int getSuggestedMinimumWidth() {
-        return (int) mBatteryWidth;
+        return (int) width;
     }
 
     @Override
     protected int getSuggestedMinimumHeight() {
-        return (int) mBatteryHeight;
+        return (int) (width * defaultRatio);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.save();
-    //    canvas.translate(measureWidth / 2, measureHeigth / 2);
-        canvas.drawRoundRect(mBatteryRect, 2f, 2f, mBatteryPaint); // 画电池轮廓需要考虑 画笔的宽度
-        canvas.drawRoundRect(mCapRect, 2f, 2f, mBatteryPaint);// 画电池盖
+        canvas.drawRoundRect(mBatteryRect, batteryArch, batteryArch, mBatteryPaint); // 画电池轮廓需要考虑 画笔的宽度
+        canvas.drawRoundRect(mCapRect, batteryArch, batteryArch, mBatteryPaint);// 画电池盖
         canvas.drawRect(mPowerRect, mPowerPaint);// 画电量
         canvas.restore();
     }
@@ -190,8 +207,7 @@ public class BatteryView extends View {
         if (mPower < 0) {
             mPower = 0;
         }
-        mPowerRect.right = mBatteryStroke / 2 + mPowerPadding
-                + mPowerWidth * ((100f - mPower) / 100f);
+        mPowerRect.right = getPaddingLeft() + mBatteryStroke + mPowerWidth * mPower / 100f;
         invalidate();
     }
 }
